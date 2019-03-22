60d209f5ae4bcaf501a94edcea8acad3daa3391b
Jacob Olin <jolin00@stuy.edu>
2019-03-22 11:20:09 -0400

    Wrote both of the constructors

--- MyDeque.java
+++ MyDeque.java
@@ -4,7 +4,13 @@ public class MyDeque<E>{
   private int start;
   private int end;
 
+  @SuppressWarnings("unchecked")
   public MyDeque() {
+    data = (E[])new Object[10];
+  }
 
+  @SuppressWarnings("unchecked")
+  public MyDeque(int initialCapacity) {
+    data = (E[])new Object[10];
   }
 }
