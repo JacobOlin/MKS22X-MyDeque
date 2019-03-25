a495dbe3b77ee63587de77fe5e5fa1270d63794d
Jacob Olin <jolin00@stuy.edu>
2019-03-25 11:43:19 -0400

    Can now distinguish between each individual character and feed it into a deque

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+
 public class Calculator{
   public static double eval(String s) {
+    ArrayList<String> a = new ArrayList<String>();
+    int j = 0;
+    for (int i = 0;i < s.length();i += 1) {
+      if (s.charAt(i) == ' ') {
+        a.add(s.substring(j,i));
+        j = i + 1;
+      }
+    }
+    a.add(s.substring(j,s.length()));
+    /*for (int i = 0;i < a.size();i += 1) {
+      System.out.print(a.get(i) + " ");
+    }
+    System.out.println();*/
     return 0.0;
+  }
+
+  public static void main(String[] args) {
+    eval(args[0]);
   }
 }
